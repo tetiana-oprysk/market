dde36dc5eb941295899a9df7291bf2774b24ffb1
Tetiana Oprysk <57996277+tetiana-oprysk@users.noreply.github.com>
2021-04-29 14:36:09 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return 'Hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
